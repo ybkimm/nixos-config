5ff86988c5a96b148589d15904cc622b34f9a483
Yongbin Kim <iam@yongbin.ki>
2024-12-18 00:32:20 +0900

    update devvm configs

--- hosts/devvm/default.nix
+++ hosts/devvm/default.nix
@@ -1,11 +1,19 @@
+let
+  add-user = import ../../utils/add-user;
+in
 {
   imports = [
     ./configuration.nix
-    (../../utils/add-user {
-      name = "ybkimm";
-      home = "/home/ybkimm";
-      config = "default";
-      extraOptions = {
+    (add-user {
+      userName = "root";
+      homePath = "/root";
+      homeConfigName = "default";
+    })
+    (add-user {
+      userName = "ybkimm";
+      homePath = "/home/ybkimm";
+      homeConfigName = "default";
+      extraUserOptions = {
         isNormalUser = true;
         extraGroups = [ "wheel" ];
       };
