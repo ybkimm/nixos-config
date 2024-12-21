45e414f00efec92f0cd75f8099d90ad7fe2bd7eb
Yongbin Kim <iam@yongbin.kim>
2024-12-21 23:37:37 +0900

    Fix curl parameter for editorconfig

--- users/ybkimm/apps/editorconfig/default.nix
+++ users/ybkimm/apps/editorconfig/default.nix
@@ -1,7 +1,7 @@
 let
   aliases = {
-    editorconfig-go = "curl -O .editorconfig https://raw.githubusercontent.com/editorconfig/editorconfig-core-go/refs/heads/master/.editorconfig";
-  }
+    editorconfig-go = "curl -o .editorconfig https://raw.githubusercontent.com/editorconfig/editorconfig-core-go/refs/heads/master/.editorconfig";
+  };
 in
 {
   programs.bash.shellAliases = aliases;
