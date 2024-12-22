cfae762a1ebcd700321b82f9b4b23d8b9ebcbcc8
Yongbin Kim <iam@yongbin.kim>
2024-12-22 17:00:18 +0900

    Add iterm2 and plemoljp-nf font

--- hosts/ybkimm-mbp/configuration.nix
+++ hosts/ybkimm-mbp/configuration.nix
@@ -2,21 +2,24 @@ inputs:
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages = [];
-  
+  environment.systemPackages = [
+    iterm2
+    plemoljp-nf
+  ];
+
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
-  
+
   # Enable alternative shell support in nix-darwin.
   # programs.fish.enable = true;
-  
+
   # Set Git commit hash for darwin-version.
   system.configurationRevision = with inputs; self.rev or self.dirtyRev or null;
-  
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
-  
+
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
 }
