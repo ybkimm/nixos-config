c8aad641d6cb4b00527cb298cf1ec67dbafc705f
Yongbin Kim <kim.yongbin.xp@mynavi.jp>
2024-12-23 17:55:34 +0900

    Open diagonostic when hold cursor

--- users/default/apps/neovim/plugins/lsp.nix
+++ users/default/apps/neovim/plugins/lsp.nix
@@ -1,6 +1,34 @@
 { pkgs, ... }:
 {
   programs.nixvim = {
+    autoCmd = [
+      {
+        event = [
+          "CursorHold"
+        ];
+        callback.__raw = ''
+          function()
+            for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+              if vim.api.nvim_win_get_config(winid).zindex then
+                return
+              end
+            end
+            vim.diagnostic.open_float({
+              scope = "cursor",
+              focusable = false,
+              close_events = {
+                "CursorMoved",
+                "CursorMovedI",
+                "BufHidden",
+                "InsertCharPre",
+                "WinLeave",
+              },
+            })
+          end
+        ''
+      }
+    ];
+
     plugins.luasnip = {
       enable = true;
     };
