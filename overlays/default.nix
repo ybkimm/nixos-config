8d09b2fc061cb4b3a2b45d3857c144e959da6833
Yongbin Kim <iam@yongbin.kim>
2025-02-02 15:43:43 +0900

    Switch to baseline binary

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,24 +10,5 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
-    bun = prev.bun.overrideAttrs (oldAttrs: rec {
-      version = "1.2.2";
-      passthru = oldAttrs.passthru // {
-        sources = {
-          "aarch64-darwin" = oldAttrs.passthru.sources."aarch64-darwin" // {
-            hash = "sha256-xNWOBsXDOIW1JvTZGjjKnr25/D+0zVR/fTMCBVyY5Bw=";
-          };
-          "aarch64-linux" = oldAttrs.passthru.sources."aarch64-linux" // {
-            hash = "sha256-0duqPpryRUn62Svb5Psh+lMwLNBIqPAE6FokCYTJPU0=";
-          };
-          "x86_64-darwin" = oldAttrs.passthru.sources."x86_64-darwin" // {
-            hash = "sha256-0duqPpryRUn62Svb5Psh+lMwLNBIqPAE6FokCYTJPU0=";
-          };
-          "x86_64-linux" = oldAttrs.passthru.sources."x86_64-linux" // {
-            hash = "sha256-P077iv0fhKwqmMBGYciYVh0dNVJ9Awy0Vx6Zt8hfUHk=";
-          };
-        };
-      };
-    });
   };
 }
