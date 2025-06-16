c9f64d922f5e906a1d983f76d5d4aba828892089
Yongbin Kim <iam@yongbin.kim>
2025-06-16 12:16:16 +0900

    Enhance kakoune configuration with plugins and custom config
    Add auto-pairs plugin and custom configuration file with fzf integration.
    Replace kakoune-lsp with fzf for better file navigation support.

--- users/default/apps/kakoune/default.nix
+++ users/default/apps/kakoune/default.nix
@@ -1,7 +1,22 @@
 { pkgs, ... }:
+let
+  kakoune = let
+    kakouneConfig = pkgs.writeTextFile (rec {
+      name = "kakrc.kak";
+      destination = "/share/kak/autoload/${name}";
+      text = builtins.readFile ./kakrc.kak;
+    });
+  in
+  pkgs.kakoune.override {
+    plugins = with pkgs.kakounePlugins; [
+      kakouneConfig
+      auto-pairs-kak
+    ];
+  };
+in
 {
-  home.packages = with pkgs; [
+  home.packages = [
     kakoune
-    kakoune-lsp
+    pkgs.fzf
   ];
 }
